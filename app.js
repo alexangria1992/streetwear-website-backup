68d64dfb635e1cee5ce3fcd41f60c6927c7e0280
Alexandre Angrignon <alex040892@gmail.com>
2024-02-28 19:33:04 +1300

    functions to update total and  remove items

--- app.js
+++ app.js
@@ -16,10 +16,83 @@ const media = window.matchMedia("(width < 1024px)");
 const topNavMenu = document.querySelector(".topnav-menu");
 
 //Shopping Cart Variables
+let cartItem = document.querySelector("#cart-item");
+let cart = document.querySelector(".cart-sidebar");
+let closeCart = document.querySelector("#close-cart");
+
+// Cart Sidebar Open
+cartItem.onclick = () => {
+  cart.classList.add("active");
+};
+
+// Cart Sidebar Close
+closeCart.onclick = () => {
+  cart.classList.remove("active");
+};
+
+// Cart working JS
+if (document.readyState == "loading") {
+  document.addEventListener("DOMContentLoaded", ready);
+} else {
+  ready();
+}
+
+// Making function
+function ready() {
+  // remove Items from Cart
+  var removeCartButtons = document.getElementsByClassName("cart-remove");
+  console.log(removeCartButtons);
+  for (var i = 0; i < removeCartButtons.length; i++) {
+    var button = removeCartButtons[i];
+    button.addEventListener("click", removeCartItem);
+  }
+  // Quantity Changes
+  var quantityInputs = document.getElementsByClassName("cart-quantity");
+  for (var i = 0; i < quantityInputs.length; i++) {
+    var input = quantityInputs[i];
+    input.addEventListener("change", quantityChanged);
+  }
+}
+
+// Remove Items From Cart
+function removeCartItem(event) {
+  var buttonClicked = event.target;
+  buttonClicked.parentElement.remove();
+  updatetotal();
+}
+
+//Quantity Changes
+function quantityChanged(event) {
+  var input = event.target;
+  if (isNaN(input.value) || input.value <= 0) {
+    input.value = 1;
+  }
+  updatetotal();
+}
+
+// Update Total
+function updatetotal() {
+  var cartContent = document.getElementsByClassName("cart-content")[0];
+  console.log(cartContent);
+  var cartBoxes = cartContent.getElementsByClassName("cart-box");
+  var total = 0;
+  for (var i = 0; i < cartBoxes.length; i++) {
+    var cartBox = cartBoxes[i];
+    var priceElement = cartBox.getElementsByClassName("cart-price")[0];
+    var quantityElement = cartBox.getElementsByClassName("cart-quantity")[0];
+    var price = parseFloat(priceElement.innerText.replace("$", ""));
+    var quantity = quantityElement.value;
+    total = total + price * quantity;
+    // If price contain some cents value
+    total = Math.round(total * 100) / 100;
+    document.getElementsByClassName("total-price")[0].innerText = "$" + total;
+  }
+}
+
 // Carousel
 
 function clickMe() {
-  console.log("ive been clicked");
+  console.log("i've been clicked");
 }
 
 let active = 0;
